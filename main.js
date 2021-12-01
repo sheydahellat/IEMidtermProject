f192f80bc83ef438488e861744f74d6367e30765
sheyda <sheydahellat@gmail.com>
2021-12-01 15:07:41 +0330

    add javascript

---
+++ main.js
@@ -0,0 +1,51 @@
+const answer = document.getElementById('gender-ans');
+const persent = document.getElementById('gender-persent');
+
+const male_choice = document.getElementById('male');
+const female_choice = document.getElementById('female');
+
+const savedAnswer = document.getElementById('saved');
+
+const saveBtn = document.getElementById('save');
+const clearBtn = document.getElementById('clear');
+
+
+// current name value
+let currentValue;
+
+
+
+
+document.getElementById('submit').addEventListener('click', () => {
+    let input = document.getElementById('name').value;
+    currentValue = input;
+    fetch(`https://api.genderize.io/?name=${input}`).then(response => response.json())
+    .then(data => {
+        answer.innerHTML = data.gender;
+        persent.innerHTML = data.probability;
+      });
+      if(localStorage.getItem(input)){
+          savedAnswer.innerHTML = localStorage.getItem(input);
+      }
+});
+
+
+
+
+saveBtn.addEventListener('click', () => {
+
+    let input = document.getElementById('name').value;
+    if(male_choice.checked || female_choice.checked){
+        localStorage.setItem(input, document.querySelector('input[name="gender"]:checked').value);
+    }
+    else{
+        localStorage.setItem(input, answer.innerHTML);
+    }
+});
+
+
+
+clearBtn.addEventListener('click', () => {
+    localStorage.removeItem(currentValue)
+    savedAnswer.innerHTML = '...';
+})
